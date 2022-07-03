d3090dd5071f7ade31d400e3eb6086e453b5ea76
Ved Gupta <vedgupta@protonmail.com>
2022-07-04 01:38:54 +0530

    Basic Backend Prototype  ready functions working videocall easily connect smoothly
    - problem in disconnecting call
    - need to implement the Web Interface
    - AndImprove the User Interface

--- pages/api/server.js
+++ pages/api/server.js
@@ -13,16 +13,40 @@ export default function handler(req, res) {
 
         io.on("connection", (socket) => {
 
+            socket.emit("me", socket.id)
+
             socket.on("chat", (msg) => {
                 socket.broadcast.emit(msg.recipent, msg);
             });
 
             socket.on("MakeCall", (data) => {
                 // call a user
+                /**
+                 * io.to(
+                        -send to specific sopcket.id
+                    ).emit(
+                        "label", data
+                    )
+                */
+                //    console.log(data);
+                PRINT(data.from)
+                socket.broadcast.emit(data.user, {
+                    caller: data.from,
+                    signal: data.signal
+                })
             });
 
             socket.on("AnswerCall", (data) => {
                 // Recieve a call
+                /**
+                 * io.to(
+                        -send to specific sopcket.id
+                    ).emit(
+                        "label", data
+                    )
+                */
+               PRINT(data.to)
+                socket.broadcast.emit(`callAccepted${data.to}`, data.signal)
             })
 
         });
--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,16 @@ export default function Home() {
   const [ callerInfo, setCallerInfo ] = useState({
     recievingCall: false,
     caller: "",
-    name: "",
     signal: null
   })
   const [ MyStream, SetMyStream ] = useState();
+  const [ username, setusername ] = useState("");
 
   const MyVideo = useRef();
   const UserVideo = useRef();
+  const connectionRef = useRef();
 
-  const socketInitializer = async () => {
+  const socketInitializer = async (username) => {
     await fetch("/api/server");
     socket = io();
 
@@ -28,11 +29,14 @@ export default function Home() {
       PRINT("connected");
     });
 
-    socket.on("MakeCall", (data) => {
+    socket.on("me", (id) => {
+      PRINT(id)
+    })
+
+    socket.on(username, (data) => {
       setCallerInfo({
         recievingCall: true,
-        caller: data.from,
-        name: data.name,
+        caller: data.caller,
         signal: data.signal
       })
     })
@@ -54,17 +58,112 @@ export default function Home() {
   }
 
 
+
   useEffect(() => {
     //  Execute on page load
+    const u = prompt("Enter the Username");
+    setusername(u);
     GetPermissionForCameraAndAudio();
-    socketInitializer();
+    socketInitializer(u);
 
   }, [])
 
+
+  const MakeCall = (to) => {
+
+    const peer = new Peer({
+      // Call Started
+      initiator: true,
+      trickle: false,
+      stream: MyStream
+    })
+
+    peer.on("signal", (data) => {
+			socket.emit("MakeCall", {
+				user: to,
+				signal: data,
+				from: username,
+			})
+		})
+
+    peer.on("stream", (stream) => {
+			UserVideo.current.srcObject = stream
+		})
+
+    PRINT(`callAccepted${username}`)
+
+    socket.on(`callAccepted${username}`, (signal) => {
+			peer.signal(signal)
+		})
+
+    connectionRef.current = peer;
+
+  }
+
+  const AnswerCall = () => {
+
+    const peer = new Peer({
+			initiator: false,
+			trickle: false,
+			stream: MyStream
+		})
+
+    peer.on("signal", (data) => {
+      PRINT(callerInfo)
+			socket.emit("AnswerCall", {to: callerInfo.caller , signal: data })
+		})
+
+    peer.on("stream", (stream) => {
+			UserVideo.current.srcObject = stream
+		})
+
+    peer.signal(callerInfo.signal)
+
+		connectionRef.current = peer;
+
+  }
+
+  const leaveCall = () => {
+    setCallerInfo({...callerInfo , recievingCall : false})
+		connectionRef.current.destroy()
+	}
+
+
   return (
     <div>
+
       <div className="myvideo">
         <video playsInline muted ref={MyVideo} autoPlay style={{ width: "300px" }} />
+      </div>
+
+      <div className="uservideo">
+        <video playsInline ref={UserVideo} autoPlay style={{ width: "300px" }} />
+      </div>
+
+      <div className="CallToSomeOne">
+        <input type="button" value="ClickToCall" onClick={()=>{
+          const to = prompt("USer To Call");
+          MakeCall(to);
+        }} />
+      </div>
+
+      <div className="acceptcall">
+        {
+          callerInfo.recievingCall ? (
+          <h1>
+            Call Comes
+            <button onClick={AnswerCall} >Recieve Call</button>
+          </h1>
+        ) : ( 
+          <h1>
+            {`${callerInfo.recievingCall} dbejkbs`}
+          </h1>
+         )
+        }
+      </div>
+
+      <div className="endCall">
+        <button onClick={leaveCall}>EndCall</button>
       </div>
     </div>
   )
